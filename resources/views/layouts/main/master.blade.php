821a6bd43cb358fe5a03c7ddc615ff67c1efee0b
Oğuz Emre Sayman <oguz.emre.sayman@gmail.com>
2024-07-27 18:39:51 +0000

    add gtag for google analytics

--- resources/views/layouts/main/master.blade.php
+++ resources/views/layouts/main/master.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="tr">
     <head>
+        <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-5QNJ9GSR9G"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-5QNJ9GSR9G');
+            </script>
+        <!-- Google tag (gtag.js) -->
         @include('layouts.main.head')
         <link rel="stylesheet" href="{{ asset('vendor/toastr/toastr.min.css') }}">
         @yield('css')
